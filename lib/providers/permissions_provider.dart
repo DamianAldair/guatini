6f9866306aa99d8c2258343c3c84959635e95abe
Damian Aldair <daldairgp@gmail.com>
2023-11-22 16:40:29 -0500

    Permissions fix

--- lib/providers/permissions_provider.dart
+++ lib/providers/permissions_provider.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -41,28 +42,41 @@ abstract class PermissionsHandler {
     return false;
   }
 
-  static Future<bool> requestStoragePermission(
+  static Future<bool> _requestStoragePermissions() async {
+    final storage = await Permission.storage.request();
+    PermissionStatus? manageExternalStorage;
+    final deviceInfo = await DeviceInfoPlugin().androidInfo;
+    if (storage.isGranted && deviceInfo.version.sdkInt >= 30) {
+      manageExternalStorage = await Permission.manageExternalStorage.request();
+    }
+    if (manageExternalStorage == null) return storage.isGranted;
+    return storage.isGranted && manageExternalStorage.isGranted;
+  }
+
+  static void requestStoragePermission(
     BuildContext context,
-    void Function() function,
+    void Function()? function,
   ) async {
-    bool status = await PermissionsHandler.status(Permission.storage);
-    if (!status) {
-      status = await PermissionsHandler.request(
-        context,
-        Permission.storage,
-      );
-    }
-    if (!status) {
-      ScaffoldMessenger.of(context).hideCurrentSnackBar();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(AppLocalizations.of(context).noStoragePermission),
-        ),
-      );
-      return false;
-    } else {
-      function.call();
-      return true;
-    }
+    _requestStoragePermissions().then((bool granted) {
+      if (granted) {
+        function?.call();
+      } else {
+        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(AppLocalizations.of(context).noStoragePermission),
+          ),
+        );
+        Future.delayed(const Duration(seconds: 2)).then((_) {
+          openAppSettings().then((bool opened) {
+            if (!opened) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text(AppLocalizations.of(context).cannotOpenSettings)),
+              );
+            }
+          });
+        });
+      }
+    });
   }
 }
